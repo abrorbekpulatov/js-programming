7d230aea6a8c0951f2915737251bff9777686e93
Abrorbek Pulatov <abrorbekpulatov@Abrorbekui-MacBook-Pro.local>
2023-02-05 20:26:37 +0900

    Chapter2: homework

---
+++ Chapter2/Homework/1.js
@@ -0,0 +1,8 @@
+// "John" qiymatini name o'zgaruvchiga belgila
+let name1 = "John";
+
+// name ning qiymatini adminga tengla
+var admin = name1;
+
+// admin nomli o'zgaruvchini console.log orqali chiqar
+console.log(admin)
---
+++ Chapter3/ex.js
@@ -0,0 +1,4 @@
+// p1 va p2 ko'paytmasini hissoblash funksiyasi 
+function myFunction(p1, p2) {
+    return p1 * p2;
+}
